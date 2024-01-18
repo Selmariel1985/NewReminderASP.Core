f6f0f979a3578ad3e280b727b26792789f2e2e8c
Григорий Фролов <shara20120@gmail.com>
2024-01-18 12:50:18 +0300

    New

---
+++ NewReminderASP.Domain/Entities/PhoneType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewReminderASP.Domain.Entities
+{
+    public class PhoneType
+    {
+        public int ID { get; set; }
+        
+        public string TypeName { get; set; } 
+    }
+}
---
+++ NewReminderASP.Services/Dtos/EventRecurrenceDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewReminderASP.Services.Dtos
+{
+    [DataContract]
+    public class EventRecurrenceDto
+    {
+        [DataMember]
+        public int ID { get; set; }
+
+        [DataMember]
+        public string RecurrenceType { get; set; }
+    }
+}
